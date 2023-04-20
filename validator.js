20db670a50465bd798def9d78bb5cbf8aac7832a
ledo9124 <leminhdo904@gmail.com>
2023-04-20 13:12:53 +0700

    finish validator

--- validator.js
+++ validator.js
@@ -1,9 +1,18 @@
 // Đối tượng validator
 function validator(options) {
 
+    let selectorRules = {};
+
     function Validate(inputElement , rule){
         let errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-        let errorMessage = rule.test(inputElement.value);
+        let errorMessage;
+
+        let rules = selectorRules[rule.selector];
+
+        for (var i in rules) {
+            errorMessage = rules[i](inputElement.value);
+            if(errorMessage) break;
+        };
 
         if(errorMessage) {
             errorElement.innerText = errorMessage;
@@ -17,15 +26,49 @@ function validator(options) {
             inputElement.parentElement.classList.add('valid');
         }
 
+        return !errorMessage;
     }
 
     let formElement = document.querySelector(options.form);
 
     if(formElement) {
 
+        formElement.onsubmit = function(e) {
+            e.preventDefault();
+
+            let isFormValid = true;
+
+            options.rules.forEach(rule => {
+                let inputElement = formElement.querySelector(rule.selector);    
+                let isValid = Validate(inputElement , rule);
+
+                if(!isValid) {
+                    isFormValid = false;
+                };
+            });
+
+            if(isFormValid){
+                if(typeof options.onSubmit === 'function') {
+                    let enableInputs = formElement.querySelectorAll('[name]:not([disable])');
+                    let formValues = Array.from(enableInputs).reduce(function(values , input){
+                        return (values[input.name] = input.value) && values;
+                    }, {});
+                    options.onSubmit(formValues);     
+                }else {
+                    formElement.submit();
+                }
+            }
+        }
+
         options.rules.forEach(rule => {
             
             let inputElement = formElement.querySelector(rule.selector); 
+
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            }else {
+                selectorRules[rule.selector] = [rule.test];
+            };    
 
             if(inputElement) {
                 //Xử lý trường hợp blur khỏi input
@@ -50,31 +93,40 @@ function validator(options) {
 
 //Định nghĩa rules
 
-validator.isRequired = function(selector) {
+validator.isRequired = function(selector , message) {
     return {
         selector: selector,
         test: function(value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này!';
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này!';
         }
     };
 }
 
-validator.isEmail = function(selector) {
+validator.isEmail = function(selector , message) {
     return {
         selector: selector,
         test: function(value) {
             const regexEmail = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-            return regexEmail.test(value) ? undefined : 'Trường này phải là email!'
+            return regexEmail.test(value) ? undefined : message || 'Trường này phải là email!'
         }
     };
 }
 
-validator.isPassword = function(selector) {
+validator.isPassword = function(selector , message) {
     return {
         selector: selector,
         test: function(value) {
             const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$/;
-            return regexPassword.test(value) ? undefined : 'Mật khẩu chưa đủ mạnh!'
+            return regexPassword.test(value) ? undefined : message || 'Mật khẩu chưa đủ mạnh!'
+        }
+    }
+}
+
+validator.isComfirmed = function(selector , getComfirmValue , message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value === getComfirmValue() ? undefined : message || 'Dữ liệu không trùng khớp!';
         }
     }
 }
